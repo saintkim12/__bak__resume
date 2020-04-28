ebf99c7aa820981c5e7bd0056cdafde3b4fa7c95
sikim <sikim@m114.co.kr>
2020-04-28 13:00:20 +0900

    css 테스트

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,54 @@
 <template>
-  <div>
-    <h1>Hello!</h1>
+  <div id="page">
+    <div class="menu">
+      <div class="menu-item">ha</div>
+      <div class="menu-item">ha</div>
+      <div class="menu-item">ho</div>
+      <div class="menu-item">huuuuuu</div>
+    </div>
+    <section>
+      <h1 style="text-align: center;">Hello!</h1>
+    </section>
+    <section>
+      <h1>Hello!</h1>
+    </section>
   </div>
 </template>
+<style lang="scss" scoped>
+#page {
+  $section-padding: 10px;
+  $menubar-width: 400px;
+  section {
+    width: calc(100% - #{$section-padding * 2});
+    height: calc(100% - #{$section-padding * 2});
+    min-height: 400px - ($section-padding * 2);
+    padding: $section-padding;
+    background-color: #eee;
+  }
+  .menu {
+    position: fixed;
+    top: 0;
+    background-color: #666;
+    width: $menubar-width;
+    left: calc((100% - #{$menubar-width}) / 2);
+    // opacity: 0.6;
+    display: flex;
+    flex-wrap: nowrap;
+    padding: 5px;
+    .menu-item {
+      margin: 2px;
+      padding: 2px;
+      // display: inline-block;
+    }
+  }
+}
+</style>
+<style lang="scss">
+html, body, #page {
+  width: 100%;
+  height: 100%;
+}
+</style>
 <script>
 import 'reset-css/reset.css'
 export default {
