dcf8ff50ce0a0923431a58af55611dfad9aee896
sikim <sikim@m114.co.kr>
2020-04-29 12:59:50 +0900

    메뉴/아이콘

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="page">
-    <div class="menu">
-      <div class="menu-item">ha</div>
-      <div class="menu-item">ha</div>
-      <div class="menu-item">ho</div>
-      <div class="menu-item">huuuuuu</div>
+    <div class="menu" :class="opened && 'opened'">
+      <div class="menu-item"><span class="mdi mdi-home"/></div>
+      <div class="menu-item"><span class="mdi mdi-account-alert"/></div>
+      <div class="menu-item"><span class="mdi mdi-email"/></div>
+      <div class="menu-item"><span class="mdi mdi-arrow-up-drop-circle-outline"/></div>
+      <div class="menu-item"><span class="mdi mdi-arrow-down-drop-circle-outline"/></div>
+      <div class="menu-item flex-right"><span class="mdi mdi-arrow-up-thick"/></div>
+      <div class="menu-item" @click="opened = false"><span class="mdi mdi-close"/></div>
     </div>
     <section>
       <h1 style="text-align: center;">Hello!</h1>
+      <div @click="opened = true">
+        Bob lives in a .
+      </div>
     </section>
     <section>
       <h1>Hello!</h1>
@@ -32,13 +38,35 @@
     width: $menubar-width;
     left: calc((100% - #{$menubar-width}) / 2);
     // opacity: 0.6;
-    display: flex;
+    display: none;
+    // transform: translateY(-100%);
+    // transition: all 0.5s ease-in-out;
     flex-wrap: nowrap;
-    padding: 5px;
+    padding: 2px;
+    &.opened {
+      display: flex;
+      // transform:translateY(0%);
+      // transition: all 0.5s ease-in-out;
+    }
     .menu-item {
+      font-size: 1.5rem;
+      -moz-user-select: none;
+      -webkit-user-select: none;
+      cursor: pointer;
       margin: 2px;
       padding: 2px;
-      // display: inline-block;
+      border: 1px solid #666;
+      border-radius: 2px;
+      color: #bbb;
+      &:hover {
+        transition-duration: .4s;
+        border: 1px solid #eee;
+        background-color: #ddd;
+        color: #000;
+      }
+      &.flex-right {
+        margin-left: auto;
+      }
     }
   }
 }
@@ -52,6 +80,11 @@ html, body, #page {
 <script>
 import 'reset-css/reset.css'
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      opened: false
+    }
+  }
 }
 </script>
